ec09d9ba3a5c501343ad551bc9a6515732783eed
MagdielM <56076033+MagdielM@users.noreply.github.com>
2020-10-21 16:37:19 -0400

    Added constructor from value and explicit cast to double. rawSymbol is now set at the NumericValue set accessor so it remains up to date.

--- NumberSymbol.cs
+++ NumberSymbol.cs
@@ -2,13 +2,32 @@
 {
     public class NumberSymbol : Symbol
     {
+        private double numericValue;
 
         public NumberSymbol(string numericString)
         {
+            level = -1;
             NumericValue = double.Parse(numericString);
             rawSymbol = numericString;
         }
 
-        public double NumericValue { get; set; }
+        public NumberSymbol(double value)
+        {
+            level = -1;
+            NumericValue = value;
+        }
+
+        public double NumericValue
+        {
+            get { return numericValue; }
+            set
+            {
+                numericValue = value;
+                rawSymbol = value.ToString();
+            }
+        }
+
+        public static implicit operator double(NumberSymbol symbol) => symbol?.numericValue ?? 0;
+        public double ToDouble() => numericValue;
     }
 }
