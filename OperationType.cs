0dec94f6d5cc34a74e78dc6b718e47b845242aed
MagdielM <56076033+MagdielM@users.noreply.github.com>
2020-10-21 16:42:59 -0400

    Added documentation.

--- OperationType.cs
+++ OperationType.cs
@@ -1,5 +1,16 @@
 ﻿namespace CalculatorCSharp
 {
+    /**
+     * <summary>
+     *   Enum that determines the type of operation to perform.
+     * </summary>
+     * <remarks>
+     *   The values are declared in reverse-precedence order to allow
+     *   <see cref="Calculator.Expression.Resolve"/> to resolve the symbols in the correct order via
+     *   simple iteration loops. Changing the order of these values could result in
+     *   undesired behavior from <see cref="Calculator.Expression.Resolve"/>
+     * </remarks>
+     */
     public enum OperationType
     {
         Add, Subtract, Multiply, Divide, Power, Invert
