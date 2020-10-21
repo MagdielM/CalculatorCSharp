94ab26cb932147ccc68a90a9740745ccd535b1a4
MagdielM <56076033+MagdielM@users.noreply.github.com>
2020-10-21 16:43:44 -0400

    Updated TODOs and added result display.

--- Program.cs
+++ Program.cs
@@ -7,8 +7,11 @@ namespace CalculatorCSharp
         static void Main(string[] args)
         {
             // TODO: Write tutorial messages.
+            // TODO: Print exceptions to console and place input prompt in loop.
             Console.WriteLine("Input expression:");
             Calculator.InitialRawExpression = Console.ReadLine();
+            Console.WriteLine($"Result: {Calculator.Resolve()}");
+            Console.ReadLine();
         }
     }
 }
